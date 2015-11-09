f772fee668e32bf1d85d8bdaa93c35241f09df8d
duanyu <qinglong61@163.com>
2015-11-09 16:06:36 +0800

    Initial Commit

---
+++ HelloWax/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWax
+//
+//  Created by duanqinglun on 15/11/9.
+//  Copyright © 2015年 duanqinglun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
